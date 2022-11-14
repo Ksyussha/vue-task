a96530d97660dc0d43f5038c132a00736baa97d5
Христюк Ксения Алексеевна <@itstep.loc>
2022-11-14 17:49:10 +0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+  <div class="texts" v-for="item in items" :key="item">
+    <p :class="{active:isActive}">меня зовут {{ item.name }} моя фамилия {{item.surname}}</p>
+    <p>мне {{item.age}} я учусь {{item.company}}</p>
+    <button  v-on:click="isActive =!isActive">Click me!</button>
+  </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items:[{name: "ксюша", surname: "христюк", age: "16", company: "шаг"}],
+      isActive: false
+    }
+  }
+}
+</script>
+
+<style>
+ .texts{
+  font-weight: 600;
+  font-size: large;
+}
+.active{
+  background-color: pink;
+  color: white;
+}
+
+</style>
